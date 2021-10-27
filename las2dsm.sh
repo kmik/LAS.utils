534182ffdd1354544f3dc7d83fee1d1f67c2c572
koomikko <mi.kukkonen@gmail.com>
2021-10-27 12:33:36 +0300

    -

--- las2dsm.sh
+++ las2dsm.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 set -f
 curDir="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-export LD_LIBRARY_PATH=/lib:/usr/lib:/usr/local/lib
-#export GDAL_DATA=$curDir/gdal/gdal_data
+export LD_LIBRARY_PATH=/lib:/usr/lib:/usr/local/lib:$curDir/gdal/java
+export GDAL_DATA=$curDir/gdal/gdal_data
+echo $LD_LIBRARY_PATH 
 
-#echo "current dir: " $curDir
-
-
-java -Djava.library.path="usr/local/lib/" -cp ".:$curDir/lib/*:$curDir/target/:$curDir/lib/gdal/" runners.RunLASutils 7 $@
+java -Djava.library.path=$curDir/gdal/java/ -cp ".:$curDir/lib/*:$curDir/target/:$curDir/lib/gdal/" runners.RunLASutils 7 $@
 
 set +f
 
