2305f00a91d3062090a294625d5241af807f19dc
Mikko Kukkonen <mi.kukkonen@gmail.com>
2021-12-08 13:23:13 +0200

    -

---
+++ lasStats.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -f
+curDir="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+export LD_LIBRARY_PATH=/lib:/usr/lib:/usr/local/lib:$curDir/gdal/java
+export GDAL_DATA=$curDir/gdal/gdal_data
+echo $LD_LIBRARY_PATH 
+
+java -Djava.library.path=$curDir/gdal/java/ -cp ".:$curDir/lib/*:$curDir/target/" runners.RunLASutils 31 $@
+
+set +f
+
